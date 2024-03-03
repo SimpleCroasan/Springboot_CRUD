5c39ae243cf0236b7f813847c8a14ed3b8fdaac5
Cristian Hernandez <cristianmanuel2304@hotmail.com>
2024-03-03 12:05:39 -0500

    .,

--- demo/src/main/java/page/demo/repository/PaisRepository.java
+++ demo/src/main/java/page/demo/repository/PaisRepository.java
@@ -3,5 +3,20 @@ package page.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import page.demo.model.Pais;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PaisRepository extends JpaRepository<Pais, Long> {
+
+    @Override
+    List<Pais> findAll();
+
+    @Override
+    <S extends Pais> S save(S entity);
+
+    @Override
+    Optional<Pais> findById(Long aLong);
+
+    @Override
+    void deleteById(Long aLong);
 }
