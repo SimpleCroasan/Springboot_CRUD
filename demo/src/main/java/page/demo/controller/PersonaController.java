31abce3aad9745a1e78900aa2d88bfa8f401e1cd
Cristian Hernandez <cristianmanuel2304@hotmail.com>
2024-03-03 14:38:29 -0500

    pequeños cambios en el metodo SavePersona

--- demo/src/main/java/page/demo/controller/PersonaController.java
+++ demo/src/main/java/page/demo/controller/PersonaController.java
@@ -3,10 +3,7 @@ package page.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import page.demo.model.Persona;
 import page.demo.service.PersonaService;
 
@@ -29,7 +26,7 @@ public class PersonaController {
     }
 
     @PostMapping("/lista")
-    private ResponseEntity<Persona> savePersona(Persona persona) {
+    private ResponseEntity<Persona> savePersona(@RequestBody Persona persona) {
         try {
             if (persona == null) {
                 return ResponseEntity.badRequest().build();
